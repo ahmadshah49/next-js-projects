906cd6bceba9ed61fa006ec5354135b4f6a1c394
ahmadshah49 <ahmadraza.fsd.pk94@gmail.com>
2023-11-18 17:59:30 +0500

    OYO

--- oyo/app/components/Header1.jsx
+++ oyo/app/components/Header1.jsx
@@ -5,23 +5,24 @@ import { PiHandbag } from "react-icons/pi";
 import { BsBuildings } from "react-icons/bs";
 import { FiPhone } from "react-icons/fi";
 import { CgProfile } from "react-icons/cg";
+import Link from 'next/link';
 
 
 const Header1 = () => {
   return (
     <div className='flex items-center justify-between px-12 h-20 border-b border-gray-300 '>
-        <Image
+      <Image
         src={"/logo.png"}
         width={400}
         height={400}
-        className='w-28  h-28' 
-        />
-        <Block className=" " heading={"OYO for Business"} title="Trusted by 5000 Corporates" icon={<PiHandbag size={25}/>}/>
-        <Block heading={"List your property"} title="Start earning in 30 mins" icon={<BsBuildings size={25}/>}/>
-        <Block heading={"45454545"} title="Call us to Book Now" icon={<FiPhone size={25}/>}/>
-        
-            <div className='flex font-semibold  border-l border-gray-200 h-full items-center justify-center px-4'><span><CgProfile size={25} className='mx-2'/></span> Login/Signup</div>
-        
+        className='w-28  h-28'
+      />
+      <Block className=" " heading={"OYO for Business"} title="Trusted by 5000 Corporates" icon={<PiHandbag size={25} />} />
+      <Block heading={"List your property"} title="Start earning in 30 mins" icon={<BsBuildings size={25} />} />
+      <Block heading={"45454545"} title="Call us to Book Now" icon={<FiPhone size={25} />} />
+
+      <Link href={"/login"}><div className='flex font-semibold  border-l border-gray-200 h-24 items-center justify-center px-4'><span><CgProfile size={25} className='mx-2' /></span> Login/Signup</div></Link>
+
     </div>
   )
 }
--- oyo/app/components/Header4.jsx
+++ oyo/app/components/Header4.jsx
@@ -16,8 +16,8 @@ const Header4 = () => {
                 </div>
             </div>
             <div>
-                    <input type="text" placeholder="e.g.,jhon@gmail.com" className="h-16 border rounded-lg px-4 outline-none" />
-                    <button className="mx-4 bg-[#F25454] hover:bg-[#f04040] duration-300 h-16 px-4 rounded-lg text-white">Notify Me</button>
+                    <input type="text" placeholder="e.g.,jhon@gmail.com" className="h-12 border rounded-lg px-4 outline-none" />
+                    <button className="mx-4 bg-[#F25454] hover:bg-[#f04040] duration-300 h-12 px-4 rounded-lg text-white">Notify Me</button>
                 </div>
         </div>
     )
